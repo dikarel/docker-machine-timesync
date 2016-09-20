60d72d0eabc05df8c9973aa8e1bce325d1b49c0a
Darius I. Karel <me@dikarel.com>
2016-09-21 05:19:55 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const DockerMachine = require('docker-machine')
+
+console.log('Syncing Docker machine clocks against pool.ntp.org every 5 minutes')
+setInterval(sync, 300000)
+sync()
+
+function sync () {
+  DockerMachine.list((err, machines) => {
+    if (err) return console.error(err)
+
+    machines.forEach(({name, state}) => {
+      if (state !== 'running') return
+
+      const machine = new DockerMachine({name})
+      const args = ['sudo', 'ntpclient', '-s', '-h', 'pool.ntp.org']
+
+      machine.ssh(args, (err) => {
+        if (err) console.error('Failed to sync', name, err)
+        console.log(name, 'clock synced')
+      })
+    })
+  })
+}
